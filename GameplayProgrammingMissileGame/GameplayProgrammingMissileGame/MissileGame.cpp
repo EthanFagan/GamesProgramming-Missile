40ed8ceab8505109f47c5c84f1a567525277222e
Ethan Fagan <C00232277@itcarlow.ie>
2018-09-26 20:40:06 +0100

    Complete

--- GameplayProgrammingMissileGame/GameplayProgrammingMissileGame/MissileGame.cpp
+++ GameplayProgrammingMissileGame/GameplayProgrammingMissileGame/MissileGame.cpp
@@ -61,7 +61,9 @@ int main()
 	Missile enemySlayer;
 	Enemy enemy;
 	//----------------------------------
-	int launchCode1 = rand() % 8999 + 1000;
+	int launchCode = rand() % 8999 + 1000;
+	int rangeVariableX = rand() % 4 + 1;
+	int rangeVariableY = rand() % 4 + 1;
 	//----------------------------------
 	enemy.coordinates.x = rand() % 50 + 1;
 	enemy.coordinates.y = rand() % 50 + 1;
@@ -69,60 +71,93 @@ int main()
 	int launchCoordinate = 0;
 	//----------------------------------
 	int playersInputtedCode = 0;
+	//----------------------------------
+	bool enemyDestroyed = false;
 
 
-	std::cout << "Select a warhead type" << std::endl << "1 = EXPLOSIVE" << std::endl << "2 = NUCLEAR" << std::endl;
-	std::cin >> warheadType;
-    
-	//-----------------------------------------------------------------------------------
-	if (warheadType == 1)
-	{
-		enemySlayer.payload = EXPLOSIVE;
-		std::cout << "payload set to EXPLOSIVE" << std::endl;
-	}
-	if (warheadType == 2)
-	{
-		enemySlayer.payload = NUCLEAR;
-		std::cout << "payload set to NUCLEAR" << std::endl;
-    }
-	else
-	{
-		std::cout << " \\ you did not choose an appropriate type, warhead has been set to explosive // " << std::endl;
-		enemySlayer.payload = EXPLOSIVE;
-	}
-	//------------------------------------------------------------------------------------
 
-	std::cout << "The enemys current location is between [" << enemy.coordinates.x << "," << enemy.coordinates.y << "]" << std::endl;
+	std::cout << "\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ MISSILE COMMAND /////////////////////" << std::endl;
 
-
-	std::cout << "Now enter the launch co-ordinates" << std::endl << "enter the X Value" << std::endl;
-	std::cin >> launchCoordinate;
-	enemySlayer.target.coordinates.x = launchCoordinate;
-	launchCoordinate = 0;
-
-	std::cout << "Now enter the Y Value" << std::endl;
-	std::cin >> launchCoordinate;
-	enemySlayer.target.coordinates.y = launchCoordinate;
-	launchCoordinate = 0;
-
-	//------------------------------------------------------------------------------------
-	while (playersInputtedCode != launchCode1)
-	{
-		std::cout << "Co-ordinates locked in" << std::endl << "To fire payload, enter the launch code [" << launchCode1 << "]" << std::endl;
-		std::cin >> playersInputtedCode;
-	}
-	//------------------------------------------------------------------------------------
 	
-	std::cout << "MISSILE LAUNCHED," << std::endl;
+	//------------------------------------------------------------------------------------
+	while (enemyDestroyed == false)
+	{
+		std::cout << "Select a warhead type" << std::endl << "1 = EXPLOSIVE" << std::endl << "2 = NUCLEAR(extended range)" << std::endl;
+		std::cin >> warheadType;
 
-	if (enemySlayer.target.coordinates.x == enemy.coordinates.x && enemySlayer.target.coordinates.y == enemy.coordinates.y)
-	{
-		std::cout << "JACKPOT" << std::endl << "target obliterated" << std::endl << "Press F to pay respects" << std::endl;
+		//-----------------------------------------------------------------------------------
+		if (warheadType == 1)
+		{
+			enemySlayer.payload = EXPLOSIVE;
+			std::cout << "payload set to EXPLOSIVE" << std::endl;
+		}
+		if (warheadType == 2)
+		{
+			enemySlayer.payload = NUCLEAR;
+			std::cout << "payload set to NUCLEAR" << std::endl;
+		}
+		else
+		{
+			std::cout << " \\ you did not choose an appropriate type, warhead has been set to explosive // " << std::endl;
+			enemySlayer.payload = EXPLOSIVE;
+		}
+    //-------------------------------------------------------------------------------------
+		std::cout << "The enemys current location is between " << enemy.coordinates.x - rangeVariableX << " and " << enemy.coordinates.x + rangeVariableX<< " on the X coordinates" << std::endl;
+
+		std::cout << "The enemys current location is between " << enemy.coordinates.y - rangeVariableY << " and " << enemy.coordinates.y + rangeVariableY << " on the Y coordinates" << std::endl;
+    //-------------------------------------------------------------------------------------
+
+		std::cout << "Now enter your target co-ordinates" << std::endl << "enter the X Value" << std::endl;
+		std::cin >> launchCoordinate;
+		enemySlayer.target.coordinates.x = launchCoordinate;
+		launchCoordinate = 0;
+
+		std::cout << "Now enter the Y Value" << std::endl;
+		std::cin >> launchCoordinate;
+		enemySlayer.target.coordinates.y = launchCoordinate;
+		launchCoordinate = 0;
+
+		//------------------------------------------------------------------------------------
+		while (playersInputtedCode != launchCode)
+		{
+			std::cout << "Co-ordinates locked in" << std::endl << "To fire payload, enter the launch code [" << launchCode << "]" << std::endl;
+			std::cin >> playersInputtedCode;
+		}
+		//------------------------------------------------------------------------------------
+
+		enemySlayer.arm();
+
+		std::cout << "MISSILE LAUNCHED," << std::endl;
+		if (enemySlayer.payload == EXPLOSIVE)
+		{
+			if (enemySlayer.target.coordinates.x == enemy.coordinates.x && enemySlayer.target.coordinates.y == enemy.coordinates.y)
+			{
+				std::cout << "JACKPOT" << std::endl << "target obliterated" << std::endl << "Press F to pay respects" << std::endl;
+				enemyDestroyed = true;
+			}
+			else
+			{
+				std::cout << "Shot missed, payload disarmed" << std::endl;
+			}
+		}
+	//---------------------------------------------------------------------------------------------
+		if (enemySlayer.payload == NUCLEAR)
+		{
+			if (enemySlayer.target.coordinates.x + 2 >= enemy.coordinates.x && enemySlayer.target.coordinates.x - 2 <= enemy.coordinates.x && enemySlayer.target.coordinates.y + 2 >= enemy.coordinates.y && enemySlayer.target.coordinates.y - 2 <= enemy.coordinates.y)
+			{
+				std::cout << "JACKPOT" << std::endl << "target obliterated" << std::endl << "Press F to pay respects" << std::endl;
+				enemyDestroyed = true;
+				enemySlayer.arm();
+			}
+			else
+			{
+				std::cout << "Shot missed , payload disarmed" << std::endl;
+				enemySlayer.arm();
+			}
+		}
+		
 	}
-	else
-	{
-		std::cout << "Shot missed, disappointing" << std::endl;
-	}
+	
 
 
 	std::system("pause");
